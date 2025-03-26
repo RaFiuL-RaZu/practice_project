f5a3fb0ead16a6fd7033cbbb99fc4f04d11d1f58
RaFiuL-RaZu <rafi102621@gmail.com>
2025-03-26 09:12:00 +0600

    innit commit

---
+++ lib/NoteApp/BackupPart/SecondPart.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class Secondpart extends StatefulWidget {
+  const Secondpart({super.key});
+
+  @override
+  State<Secondpart> createState() => _SecondpartState();
+}
+
+class _SecondpartState extends State<Secondpart> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
